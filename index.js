cfba88fa6653da76dcdbabc0cdb29b17f506b00e
eemilyy <emilyalmeeidas@gmail.com>
2023-08-03 14:29:52 -0300

    adicionando Pessoa na API

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express =  require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+const Person = require('./models/Person');
+
 // enviar e ler JSON
 app.use(
     express.urlencoded({
@@ -10,6 +12,31 @@ app.use(
 )
 
 app.use(express.json()); 
+
+app.post('/person', async(req, res)=>{
+
+    const {name, salary, approved} = req.body;
+
+    if(!name){
+        res.status(422).json({message: 'Insert name'})
+    }
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+    try {
+        await Person.create(person)
+
+        res.status(201).json({person});
+
+    } catch (error) {
+        res.status(500).json({error: error});        
+    }
+
+})
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello World'})
